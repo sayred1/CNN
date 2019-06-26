fixed backprop

CNN w Numpy.py
@@ -8,17 +8,6 @@ import backward
 from tensorflow import keras
 from tensorflow.keras import datasets
 
-# load the data
-(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
-train_images = train_images.reshape((60000, 28, 28, 1)) # (samples, rows, cols, channels)
-test_images = test_images.reshape((10000, 28, 28, 1)) # (samples, rows, cols, channels)
-
-# simple normalization of the image pixel values
-train_images, test_images = train_images / 255.0, test_images / 255.0
-
-train_image = train_images[0].reshape(1,28,28,1) # one img x 28 pixels x 28 pixels x one channel
-train_label = train_labels[0]
-
 class my_CNN(object):
     
     def __init__(self):
@@ -35,12 +24,13 @@ class my_CNN(object):
         if name == 'conv1':
             self.forward_dict['image'] = image
         output = utilz.get_output(self, image, kernel_dim, stride, num_filters)
+        weight, bias = utilz.get_parameters(self, image, kernel_dim)
         numImages, heightImages, widthImages, depthImages = output.shape
         weights = []
         biases = []
         for i in range(numImages):
             for l in range(depthImages):
-                weight, bias = utilz.get_parameters(self, image,kernel_dim) # weights change for each depth slice
+                weight, bias = utilz.get_parameters(self, image, kernel_dim) # weights change for each depth slice
                 height = 0
                 for j in range(heightImages): 
                     width = 0
@@ -49,7 +39,7 @@ class my_CNN(object):
                         width += stride
                     height += stride
                 weights.append(weight)
-        
+
         # store weights, biases and neuron outputs
         self.weight_dict[name] = weights
         self.bias_dict[name] = bias
@@ -94,7 +84,8 @@ class my_CNN(object):
             # fully_connected 1
             weight = np.random.randn(64,1600) * 0.01
             bias = np.random.randn(1) * 0.01
-            f1 = self.relu(weight.dot(p2_flat.transpose())+bias)
+            z1 = weight.dot(p2_flat.transpose())+bias
+            f1 = utilz.relu(self , z1)
             self.forward_dict['f1'] = f1  
             self.weight_dict['f1'] = weight
             self.bias_dict['f1'] = bias  
@@ -102,39 +93,45 @@ class my_CNN(object):
             # fully_connected 2
             weight = np.random.randn(10,64) * 0.01
             bias = np.random.randn(1) * 0.01
-            f2 = self.softmax(weight.dot(f1)+bias)
+            z2 = weight.dot(f1) + bias
+            f2 = utilz.softmax(self, z2)
             self.forward_dict['f2'] = f2
             self.weight_dict['f2'] = weight
             self.bias_dict['f2'] = bias
             
-            ######################################
-            ############# Backprop ###############
-            ######################################
             """
-            hn = conv, pn = pool, fn = fc
-            cn = activ. function(hn)
+            zn = fully conn., fn = activation(zn)
+            pn = pool
+            hn = conv, cn = activation(hn)
             kn = conv. kernels, bn = bias, wn = fc weights
             """
             
-            X,h1,p1,h2,p2= self.get_forward_dict()
-            c1,c2 = self.relu(h1),self.relu(h2)
+            X,h1,p1,h2,p2= utilz.get_forward_dict(self)
+            c1,c2 = utilz.relu(self,h1),utilz.relu(self,h2)
             f1,f2 = self.forward_dict['f1'],self.forward_dict['f2']
-            k1,b1,k2,b2,w3,b3,w4,b4 = self.get_parameter_dict()
+            k1,b1,k2,b2,w3,b3,w4,b4 = utilz.get_parameter_dict(self)
             
+            ######################################
+            ############# Backprop ###############
+            ######################################
+
             # print keys for reference:
             print('forward keys:', self.forward_dict.keys())
             print('weight keys:', self.weight_dict.keys())
-            print('bias keys:',self.bias_dict.keys())
-            print('pooling keys:',self.unit_dict.keys())
+            print('bias keys:', self.bias_dict.keys())
+            print('pooling keys:', self.unit_dict.keys())
 
             loss = backward.loss(self, f2, _image) 
             
+            for layers in self.forward_dict.keys():
+                
             # fc second gradient
             gradfc2, gradw4, gradb4 = backward.fc_grad_second(self, _image, f2, f1,)
             
             # fc first gradient
+            print(z1.shape)
             gradfc1, gradw3, gradb3 = backward.fc_grad_first(self, gradfc2, w4, p2_flat)
-                        
+            
             # flattened pool 2 gradient
             gradp2 = w3.T.dot(gradfc1)
             gradp2 = np.reshape(gradp2, p2.shape)
@@ -151,24 +148,22 @@ class my_CNN(object):
             gradc1[conv1<=0] = 0
             
             # image gradient 
-            dimage, gradk1, gradb1 = backward.convolution_gradient(self,gradc1, X, k1, b1, 3, 1)
-            
+            gradimage, gradk1, gradb1 = backward.convolution_gradient(self,gradc1, X, k1, b1, 3, 1)
 
-    def relu(self,image):
-        return np.maximum(0,image)
-    
-    def softmax(self, image):
-        return np.exp(image)/np.sum(np.exp(image))
-    
-    def get_forward_dict(self):
-        X,h1,h2 = self.forward_dict['image'],self.forward_dict['conv1'],self.forward_dict['conv2']
-        p1,p2 = self.forward_dict['pool1'],self.forward_dict['pool2']
-        return (X,h1,p1,h2,p2)
-    
-    def get_parameter_dict(self):
-        k1,k2,w1,w2 = self.weight_dict['conv1'][0],self.weight_dict['conv2'][0],self.weight_dict['f1'],self.weight_dict['f2']
-        b1,b2,b1_f,b2_f = self.weight_dict['conv1'],self.weight_dict['conv2'],self.weight_dict['f1'],self.weight_dict['f2']
-        return (k1,b1,k2,b2,w1,b1_f,w2,b2_f)
+            grads = [gradw4,gradb4,gradw3,gradb3,gradk2,gradb2,gradk1,gradb1]
+            
+            
+# load the data
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+train_images = train_images.reshape((60000, 28, 28, 1)) # (samples, rows, cols, channels)
+test_images = test_images.reshape((10000, 28, 28, 1)) # (samples, rows, cols, channels)
+
+# simple normalization of the image pixel values
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+train_image = train_images[0].reshape(1,28,28,1) # one img x 28 pixels x 28 pixels x one channel
+train_label = train_labels[0] 
+
 
 conv1 = my_CNN().convolve(train_image,3,1,64,'conv1')
 pool1 = my_CNN().pool(conv1,train_label,2,2,'pool1',False)

backward.py
@@ -1,4 +1,6 @@
 import numpy as np
+import utilz
+
 
 def loss(self, output, label):
     
@@ -102,10 +104,9 @@ def fc_grad_second(self, label, output, prev_input):
     gradb4 = np.sum(gradf2, axis = 0)
     return(gradf2,gradw4,gradb4)
 
-def fc_grad_first(self, gradfc2, weight, prev_input):
+def fc_grad_first(self, gradfc2, weight, prev_input, curr_input):
     f1 = self.forward_dict['f1']
-    gradf1 = weight.T.dot(gradfc2)
-    gradf1[f1<=0] = 0
+    gradf1 = weight.T.dot(gradfc2) * utilz.gradRelu(self, curr_input)
     gradw3 = gradf1.dot(prev_input)
     gradb3 = np.sum(gradf1,axis = 0)
     return(gradf1, gradw3, gradb3)